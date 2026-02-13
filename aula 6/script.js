011fdaee66580061115df2962efb368626014e7e
nathanguedesbarreto81-oss <nathanguedesbarreto81@gmail.com>
2026-02-12 21:10:03 -0300

    Add files via upload

---
+++ aula 6/script.js
@@ -0,0 +1,35 @@
+
+
+function soma(){
+let numero1=document.getElementById("n1").value
+let numero2=document.getElementById("n2").value
+
+resultado = parseFloat(numero1)+parseFloat(numero2)
+document.getElementById("resultado").innerHTML=resultado
+}
+
+function sub(){
+let numero1=document.getElementById("n1").value
+let numero2=document.getElementById("n2").value
+
+resultado = parseFloat(numero1)-parseFloat(numero2)
+document.getElementById("resultado").innerHTML=resultado
+}
+
+function sub(){
+let numero1=document.getElementById("n1").value
+let numero2=document.getElementById("n2").value
+
+resultado = parseFloat(numero1)/parseFloat(numero2)
+document.getElementById("resultado").innerHTML=resultado
+}
+
+
+function mutiplicacao(){
+let numero1=document.getElementById("n1").value
+let numero2=document.getElementById("n2").value
+
+resultado = parseFloat(numero1)*parseFloat(numero2)
+document.getElementById("resultado").innerHTML=resultado
+}
+
