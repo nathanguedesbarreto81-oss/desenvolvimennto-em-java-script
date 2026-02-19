b2e7f6c4dbdc1917c4cc17c6cad1dcb75cdde88b
nathanguedesbarreto81-oss <nathanguedesbarreto81@gmail.com>
2026-02-18 21:11:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-script.js
 
-let campoTexto = document.getElementById("texto");
-let campoCor = document.getElementById("cor");
-let resultado = document.getElementById("resultado");
+let nome = prompt('digite aqui seu nome: ')
+let dado =document.getElementById("texto".valueOf.String)
+let btn= document.getElementById('btn')
+if (nome =='julia'){
+    if(dado ='julia')
+    
+    console.log('seja bem vinda', nome)
+alert('seja bem vinda')
+btn.addEventListener("click",function ()
 
-
-let botaoExibir = document.getElementById("btnExibir");
-let botaoLimpar = document.getElementById("btnLimpar");
-
-
-botaoExibir.addEventListener("click", function () {
-    resultado.innerText = campoTexto.value;
-    resultado.style.color = campoCor.value;
-});
+ 
 
 
-botaoLimpar.addEventListener("click", function () {
-    resultado.innerText = "";
-});
+) 
+
+  alert('seja bem vindo(a)' +String)
+}else{
+else if (dado == "carlos"){
+
+
+console.log{
+
+
+}
+}
+console.log('seja bem vindo(a)', + nome)
+
+}
+let ano_nacimento = prompt()
+if (ano_nacimento)´
+if (cidade ==='guarulhod'){
+
+console.log('nossa cidade')
+
+
+}
+{let modelo = prompt('digite o modelo do veiculo:')
+let valor = prompt('qual cor voce acha interessante')
+
+if (modelo === 'jeep' && valor <= 70000 && corVeiculo == 'preta')
+ } console.log('mande a prposta no financiamento!')
