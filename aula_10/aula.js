a41c7cd5c79b968af19f92141dfa4433f4b8d26b
nathanguedesbarreto81-oss <nathanguedesbarreto81@gmail.com>
2026-03-04 19:44:49 -0300

    Add files via upload

---
+++ aula_10/aula.js
@@ -0,0 +1,26 @@
+function saudacao(nome,horario) {
+     
+let saudaca_o = prompt('manha,tarde,noite')
+ 
+
+   if (saudaca_o=='manha'){
+return nome + saudaca_o
+
+
+   }
+
+else if (saudaca_o=='tarde'){
+
+
+return nome + saudaca_o
+
+}
+if  (saudaca_o=='noite'){
+
+
+return nome + saudaca_o
+
+
+
+}
+}
